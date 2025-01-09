f3b1cf9df57d0d6bc29ad885246ab30342e6acdb
Dorikku <nifashendrickson@gmail.com>
2025-01-09 22:24:48 +0800

    💄better regions UI with database

--- kakaninai/lib/screens/inner_screens/region_details.dart
+++ kakaninai/lib/screens/inner_screens/region_details.dart
@@ -1,4 +1,6 @@
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
 import 'package:flutter/material.dart';
+import 'package:kakaninai/widgets/kakanin/kakanin_widget.dart';
 import 'package:kakaninai/screens/inner_screens/kakanin_details.dart';
 
 // SQL package
@@ -55,8 +57,10 @@ class _RegionDetailsState extends State<RegionDetails> {
   Future<List<Map<String, dynamic>>> fetchData() async {
     final db = await initializeDatabase();
     return await db.rawQuery('''
-      SELECT kakanin_id, kakanin_name FROM kakanin 
-      WHERE region_id = ?''', [widget.region['region_id']]);
+      SELECT *, image_file_location AS image
+      FROM kakanin, provinces
+      WHERE kakanin.province_id = provinces.province_id 
+      AND kakanin.region_id = ?''', [widget.region['region_id']]);
   }
 
   Future<void> fetchDataFromDatabase() async {
@@ -72,77 +76,157 @@ class _RegionDetailsState extends State<RegionDetails> {
       appBar: AppBar(
         title: Text(widget.region['region_name']!),
       ),
+      // body: Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: Column(
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     children: [
+      //       Text(
+      //         widget.region['region_name']!,
+      //         style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      //       ),
+      //       const SizedBox(height: 16),
+      //       // for (var item in data) Text(item['kakanin_name']),
+      //       // ListView.builder(
+      //       //   // itemCount: data.length,
+      //       //   itemCount: 1,
+      //       //   padding: const EdgeInsets.symmetric(vertical: 8.0),
+      //       //   itemBuilder: (context, index) {
+      //       //     // final region = data[index];
+      //       //     return Card(
+      //       //         margin: const EdgeInsets.symmetric(
+      //       //             vertical: 8.0, horizontal: 16.0),
+      //       //         shape: RoundedRectangleBorder(
+      //       //           borderRadius: BorderRadius.circular(12.0),
+      //       //         ),
+      //       //         elevation: 4.0,
+      //       //         child: GestureDetector(
+      //       //           // onTap: () {
+      //       //           //   Navigator.push(
+      //       //           //     context,
+      //       //           //     MaterialPageRoute(
+      //       //           //       builder: (context) => KakaninDetails(kakanin: region),
+      //       //           //     ),
+      //       //           //   );
+      //       //           // },
+      //       //           child: ListTile(
+      //       //             leading: ClipRRect(
+      //       //               borderRadius: BorderRadius.circular(8.0),
+      //       //               // child: Image.asset(
+      //       //               //   region['image']!,
+      //       //               //   // 'images/region_images/ncr-1.png',
+      //       //               //   // 'https://via.placeholder.com/150',
+      //       //               //   width: 50,
+      //       //               //   height: 50,
+      //       //               //   fit: BoxFit.cover,
+      //       //               //   color: const Color.fromARGB(255, 247, 242, 250),
+      //       //               //   colorBlendMode: BlendMode.multiply,
+      //       //               //   // loadingBuilder: (context, child, loadingProgress) {
+      //       //               //   //   if (loadingProgress == null) return child;
+      //       //               //   //   return const Center(
+      //       //               //   //     child: CircularProgressIndicator(),
+      //       //               //   //   );
+      //       //               //   // },
+      //       //               //   // errorBuilder: (context, error, stackTrace) {
+      //       //               //   //   return const Icon(
+      //       //               //   //     Icons.broken_image,
+      //       //               //   //     size: 50,
+      //       //               //   //     color: Colors.grey,
+      //       //               //   //   );
+      //       //               //   // },
+      //       //               // ),
+      //       //             ),
+      //       //             title: const Text(
+      //       //               'TITEEE', // FIX THIS
+      //       //               style: TextStyle(
+      //       //                 fontSize: 16.0,
+      //       //                 fontWeight: FontWeight.bold,
+      //       //               ),
+      //       //             ),
+      //       //             trailing: const Icon(Icons.arrow_forward_ios),
+      //       //           ),
+      //       //         ));
+      //       //   },
+      //       // ),
+      //     ],
+      //   ),
+      // ),
+      // body: ListView.builder(
+      //   itemCount: data.length,
+      //   // itemCount: 1,
+      //   padding: const EdgeInsets.symmetric(vertical: 8.0),
+      //   itemBuilder: (context, index) {
+      //     final kakanin = data[index];
+      //     // print(kakanin['kakanin_name']);
+      //     return Card(
+      //         margin:
+      //             const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      //         shape: RoundedRectangleBorder(
+      //           borderRadius: BorderRadius.circular(12.0),
+      //         ),
+      //         elevation: 4.0,
+      //         child: GestureDetector(
+      //           // onTap: () {
+      //           //   Navigator.push(
+      //           //     context,
+      //           //     MaterialPageRoute(
+      //           //       builder: (context) => KakaninDetails(kakanin: region),
+      //           //     ),
+      //           //   );
+      //           // },
+      //           child: ListTile(
+      //             leading: ClipRRect(
+      //               borderRadius: BorderRadius.circular(8.0),
+      //               // child: Image.asset(
+      //               //   region['image']!,
+      //               //   // 'images/region_images/ncr-1.png',
+      //               //   // 'https://via.placeholder.com/150',
+      //               //   width: 50,
+      //               //   height: 50,
+      //               //   fit: BoxFit.cover,
+      //               //   color: const Color.fromARGB(255, 247, 242, 250),
+      //               //   colorBlendMode: BlendMode.multiply,
+      //               //   // loadingBuilder: (context, child, loadingProgress) {
+      //               //   //   if (loadingProgress == null) return child;
+      //               //   //   return const Center(
+      //               //   //     child: CircularProgressIndicator(),
+      //               //   //   );
+      //               //   // },
+      //               //   // errorBuilder: (context, error, stackTrace) {
+      //               //   //   return const Icon(
+      //               //   //     Icons.broken_image,
+      //               //   //     size: 50,
+      //               //   //     color: Colors.grey,
+      //               //   //   );
+      //               //   // },
+      //               // ),
+      //             ),
+      //             title: Text(
+      //               kakanin['kakanin_name'], // FIXED
+      //               style: const TextStyle(
+      //                 fontSize: 16.0,
+      //                 fontWeight: FontWeight.bold,
+      //               ),
+      //             ),
+      //             trailing: const Icon(Icons.arrow_forward_ios),
+      //           ),
+      //         ));
+      //   },
+      // ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              widget.region['region_name']!,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            for (var item in data) Text(item['kakanin_name']),
-            ListView.builder(
-              itemCount: data.length,
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              itemBuilder: (context, index) {
-                final region = data[index];
-                return Card(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 16.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
-                    elevation: 4.0,
-                    child: GestureDetector(
-                      // onTap: () {
-                      //   Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //       builder: (context) => KakaninDetails(kakanin: region),
-                      //     ),
-                      //   );
-                      // },
-                      child: ListTile(
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.asset(
-                            region['image']!,
-                            // 'images/region_images/ncr-1.png',
-                            // 'https://via.placeholder.com/150',
-                            width: 50,
-                            height: 50,
-                            fit: BoxFit.cover,
-                            color: const Color.fromARGB(255, 247, 242, 250),
-                            colorBlendMode: BlendMode.multiply,
-                            // loadingBuilder: (context, child, loadingProgress) {
-                            //   if (loadingProgress == null) return child;
-                            //   return const Center(
-                            //     child: CircularProgressIndicator(),
-                            //   );
-                            // },
-                            // errorBuilder: (context, error, stackTrace) {
-                            //   return const Icon(
-                            //     Icons.broken_image,
-                            //     size: 50,
-                            //     color: Colors.grey,
-                            //   );
-                            // },
-                          ),
-                        ),
-                        title: Text(
-                          data, // FIX THIS
-                          style: const TextStyle(
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        trailing: const Icon(Icons.arrow_forward_ios),
-                      ),
-                    ));
-              },
-            ),
+            Expanded(
+              child: DynamicHeightGridView(
+                  builder: (context, index) {
+                    return KakaninWidget(
+                      kakanin: data[index],
+                    );
+                  },
+                  itemCount: data.length,
+                  crossAxisCount: 2),
+            )
           ],
         ),
       ),
